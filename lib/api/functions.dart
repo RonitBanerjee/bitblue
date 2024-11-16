683764af937cbe7127cc2f0ea58774b81b110d03
ronit <ronit@sambuq.com>
2024-11-16 21:51:28 +0530

    refactoring

--- lib/api/functions.dart
+++ lib/api/functions.dart
@@ -1,17 +1,29 @@
-// import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// class Functions {
-//    Future<void> checkForUpdate({String currentVersion}) async {
-//     try {
-//       final response = await http.get(Uri.parse(updateUrl));
-//       if (response.statusCode == 200) {
-//         final versionData = json.decode(response.body);
-//         if (versionData['latest_version'] != currentVersion) {
-//           return versionData;
-//         }
-//       }
-//     } catch (e) {
-//       print("Error checking for updates: $e");
-//     }
-//   }
-// }
+import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
+
+class Functions {
+  static String currentVersion = "1.0.0";
+  static String updateUrl =
+      "https://demo-uxapollo.s3.eu-north-1.amazonaws.com/bitblue.json";
+
+  static Future<dynamic> checkForUpdate() async {
+    try {
+      final response = await http.get(Uri.parse(updateUrl));
+      if (response.statusCode == 200) {
+        final versionData = json.decode(response.body);
+        if (versionData['latest_version'] != currentVersion) {
+          print('New Version is available: ${versionData['latest_version']}');
+          return versionData;
+        }
+      }
+    } catch (e) {
+      print("Error checking for updates: $e");
+    }
+  }
+
+    static Future<void> requestPermissions() async {
+    await Permission.storage.request();
+  }
+}
--- lib/body.dart
+++ lib/body.dart
@@ -1,11 +1,5 @@
-import 'dart:convert';
-import 'package:dio/dio.dart';
+import 'package:bitblue/api/functions.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-// import 'package:install_plugin/install_plugin.dart';
-import 'package:open_file/open_file.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Body extends StatefulWidget {
@@ -16,40 +10,87 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  final String currentVersion = "1.0.0";
-  final String updateUrl =
-      "https://demo-uxapollo.s3.eu-north-1.amazonaws.com/bitblue.json";
-
   @override
   void initState() {
     super.initState();
-    _checkForUpdate();
+    Functions.checkForUpdate();
   }
 
-  Future<String> _getLocalFilePath() async {
-    final directory = await getExternalStorageDirectory();
-    return "${directory!.path}/app-release.apk";
-  }
 
-  Future<void> _requestPermissions() async {
-  await Permission.storage.request();
-}
 
-  Future<void> _checkForUpdate() async {
+  void _downloadAndInstallApk(String apkUrl) async {
+    await Functions.requestPermissions();
     try {
-      final response = await http.get(Uri.parse(updateUrl));
-      if (response.statusCode == 200) {
-        final versionData = json.decode(response.body);
-        if (versionData['latest_version'] != currentVersion) {
-          print(versionData['apk_url']);
-          _showUpdateDialog(versionData['apk_url']);
-        }
-      }
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return const Center(child: CircularProgressIndicator());
+        },
+      );
+
+      // Close the loading indicator
+      Navigator.pop(context);
+
+      // Open the downloaded APK file
+      launchUrl(Uri.parse(apkUrl));
     } catch (e) {
-      print("Error checking for updates: $e");
+      Navigator.pop(context);
+      print("Error downloading or opening APK: $e");
     }
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'This is the base version of BitBlue',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          const Text(
+            'v1.0.0',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueGrey,
+            ),
+          ),
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () async {
+                print('This is here...');
+                var versionData = await Functions.checkForUpdate();
+                if (versionData != null) {
+                  _showUpdateDialog(versionData['apk_url']);
+                }
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 8),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(8)),
+                child: const Text('Check For Updates'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+  //This is the code for the dialog
   void _showUpdateDialog(String apkUrl) {
     showDialog(
       context: context,
@@ -73,81 +114,6 @@ class _BodyState extends State<Body> {
           ],
         );
       },
-    );
-  }
-
-  void _downloadAndInstallApk(String apkUrl) async {
-    await _requestPermissions();
-    try {
-      showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return const Center(child: CircularProgressIndicator());
-        },
-      );
-
-      // Get the path to save the downloaded APK
-      String filePath = await _getLocalFilePath();
-
-      // Download the APK using Dio
-      Dio dio = Dio();
-      await dio.download(apkUrl, filePath);
-
-      // Close the loading indicator
-      Navigator.pop(context);
-
-      // Open the downloaded APK file
-      launchUrl(Uri.parse(apkUrl));
-    } catch (e) {
-      Navigator.pop(context); // Close the loading indicator if there's an error
-      print("Error downloading or opening APK: $e");
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            'This is the base version of BitBlue',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-           Text(
-            'v$currentVersion',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueGrey,
-            ),
-          ),
-          MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: GestureDetector(
-              onTap: () async {
-                print('This is here...');
-                await _checkForUpdate();
-              },
-              child: Container(
-                margin: const EdgeInsets.only(top: 8),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(8)),
-                child: const Text('Check For Updates'),
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }

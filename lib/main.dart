4215e8dce41abc28ad12d90156cf156a55393918
ronit <ronit@sambuq.com>
2024-11-16 20:20:36 +0530

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:bitblue/body.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
